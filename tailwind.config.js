ba00f90561aec8bd1cf39361c8ebd3610230626b
manhellis <manh@manhellis.com>
2023-12-06 17:18:46 -0800

    last min fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif']
+        'poppins': ['Poppins', 'sans-serif'],
+        'neuropol': ['neuropol-nova', 'sans-serif']
       },
+      backgroundImage: {
+        'banner1': "url('./img/bg-1.jpeg')",
+        'banner2': "url('./img/bg-2.jpeg')",
+        'banner3': "url('./img/bg-3.jpeg')",
+       },
     },
   },
   plugins: [],
